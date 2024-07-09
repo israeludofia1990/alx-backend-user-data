bc3382ce29a81c902b6a323e5b4730cfd904597b
israeludofia1990 <udofiaisrael170@gmail.com>
2024-07-09 14:03:56 +0100

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 from api.v1.auth.auth import Auth
 from base64 import b64decode
 import binascii
+from models.user import User
+from typing import TypeVar, Tuple
 
 
 class BasicAuth(Auth):
@@ -34,3 +36,28 @@ class BasicAuth(Auth):
             return None
         decoded_str = decoded_bytes.decode('utf-8')
         return decoded_str
+
+    def extract_user_credentials(
+            self, decoded_b64_auth_header: str) -> (str, str):
+        """ Returns user credentials """
+        if decoded_b64_auth_header is None or not isinstance(
+                decoded_b64_auth_header, str) \
+           or ':' not in decoded_b64_auth_header:
+            return (None, None)
+        return decoded_b64_auth_header.split(':', 1)
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """ Returns user object from credentials """
+        if user_email is None or not isinstance(
+                user_email, str) or user_pwd is None or not isinstance(
+                    user_pwd, str):
+            return None
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
+            return None
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+            return None
