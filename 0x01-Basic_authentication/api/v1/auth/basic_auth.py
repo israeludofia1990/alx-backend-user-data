f49713d3f19f090307643fad87b25a17098ec37d
israeludofia1990 <udofiaisrael170@gmail.com>
2024-07-09 13:46:06 +0100

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 '''basic auth'''
 from api.v1.auth.auth import Auth
+from base64 import b64decode
+import binascii
 
 
 class BasicAuth(Auth):
@@ -18,3 +20,17 @@ class BasicAuth(Auth):
             return None
         base64_part = authorization_header[6:]
         return base64_part
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        '''decode base64 authorization header'''
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = b64decode(base64_authorization_header)
+        except binascii.Error:
+            return None
+        decoded_str = decoded_bytes.decode('utf-8')
+        return decoded_str
