a5b4089ec4543ececde9f66d3c5adae7f24e8ddc
israeludofia1990 <udofiaisrael170@gmail.com>
2024-07-12 02:56:50 +0100

    Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,8 +5,17 @@ from base64 import b64decode
 import binascii
 from models.user import User
 from typing import TypeVar, Tuple
+import uuid
 
 
 class SessionAuth(Auth):
     '''session auth'''
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        '''creates a Session ID for a user_id'''
+        if not user_id or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        user_id_by_session_id[session_id] = user_id
+        return session_id
